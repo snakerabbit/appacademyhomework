300c188174dc59c3ab1fa2f0ad115542215802c4
alisoncheng90 <alison.s.cheng@gmail.com>
2017-08-03 23:18:56 -0700

    completed queue class

---
+++ W1D5/queue.rb
@@ -0,0 +1,18 @@
+class Queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue (value)
+    @queue.push(value)
+  end
+
+  def dequeue (value)
+    @queue.shift(value)
+  end
+
+  def show
+    @queue
+  end
+  
+end
